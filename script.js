f8af341483ab396c1440cb2558c8370447d3c263
Guilherme Menrraz <65176106+Menrraz@users.noreply.github.com>
2022-01-05 16:46:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,15 +12,11 @@ function addItem(keyItem, keyQuantity) {
             <p class='item-p item-p1'>${localStorage.getItem(keyItem)}</p>
             <p class='item-p item-p2'>${localStorage.getItem(keyQuantity)}</p>
             <div class='div-icons'>
-
                 <i class="fas fa-shopping-cart icon" 
-                ontouchstart="addToCart('${'item' + k}','${'quantity' + k}', 'icon')" 
                 onclick="addToCart('${'item' + k}','${'quantity' + k}', 'icon')">
                 </i>
-
                 <i class="fas fa-trash-alt icon" id='toCartId' 
                 onclick="removeItem('${'item' + k}', '${'quantity' + k}')"
-                ontouchstart="removeItem('${'item' + k}', '${'quantity' + k}')"
                 title='Remove Item'></i>
             </div>
         </div>`) : '' // To not create useless divs and get in my way on CSS styles
